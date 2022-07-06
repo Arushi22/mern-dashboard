6c10972a1b3f1d8b2eceb97ab159ca9fc02926af
arushi sharma <sharmaarushi424@gmail.com>
2022-07-06 23:41:44 +0530

    fixed the sub repo

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <h3>e-commerce footer</h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/SignUp.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="register">
+      <h1>Signup</h1>
+      <input className="inputBox" type="text" placeholder="Enter Name" />
+      <input className="inputBox" type="mail" placeholder="Enter Email" />
+      <input
+        className="inputBox"
+        type="password"
+        placeholder="Enter Password"
+      />
+      <button className="appButton">SignUp</button>
+    </div>
+  );
+}
+
+export default SignUp;
